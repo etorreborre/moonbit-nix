36adfa2941c9225236f0441f54133581d17688f8
Bikal Lem <gbikal@gmail.com>
2024-10-05 13:41:14 +0100

    add rescript compiler dev env

--- flake.nix
+++ flake.nix
@@ -14,8 +14,14 @@
     }:
     let
       system = "x86_64-linux";
-      pkgs = import nixpkgs { inherit system; };
       moonbit = pkgs.callPackage ./pkgs/moonbit.nix { };
+      ocaml_overlay = final: prev: {
+        ocamlPackages = prev.ocaml-ng.ocamlPackages_5_2;
+      };
+      pkgs = import nixpkgs {
+        inherit system;
+        overlays = [ ocaml_overlay ];
+      };
     in
     {
       packages.${system} = rec {
@@ -23,9 +29,25 @@
         default = moonbit;
       };
 
-      devShells.${system} = {
-        default = pkgs.mkShell rec {
+      devShells.${system} = rec {
+        default = moonbit-dev;
+        moonbit-dev = pkgs.mkShell {
           packages = [ moonbit ];
+        };
+        rescript-compiler-dev = pkgs.mkShell {
+          nativeBuildInputs = [ pkgs.ocamlPackages.ocaml ];
+          buildInputs = with pkgs.ocamlPackages; [
+            dune_3
+            ocamlformat_0_26_2
+            cppo
+            ounit2
+            js_of_ocaml
+
+            # node
+            pkgs.ninja
+            pkgs.nodejs
+            pkgs.cargo
+          ];
         };
       };
     };
